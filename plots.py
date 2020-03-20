36dcbbb02badc0b31178653711c116c0c6be07ad
David Alonso <dam.phys@gmail.com>
2020-03-20 16:32:21 +0000

    new plots

--- plots.py
+++ plots.py
@@ -2,236 +2,204 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyccl as ccl
 from scipy.interpolate import interp1d
-from utils import UHECRs, Gals
-
+from utils import UHECRs, Gals, get_cl
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
+cols = ['#0000AA', '#AAAA00', '#AA0000']
+us = []
+us.append(UHECRs(100, "data/attenuation_A1_e10.0_E20.0_g2.6.dat", 30, 1.))
+us.append(UHECRs(59, "data/attenuation_A1_e10.0_E20.0_g2.6.dat", 200, 1.))
+us.append(UHECRs(39, "data/attenuation_A1_e10.0_E20.0_g2.6.dat", 1000, 1.))
+g = Gals("2MRS", 1E5)
 
-n_gal = 1E5
-n_crs = 1E2
 plot_nz = True
 plot_cl_nl = True
 plot_cl_el = True
 plot_sn = True
-ells = np.unique(np.geomspace(2, 1000, 100).astype(int)).astype(float)
 ls_all = np.arange(2, 1000)
 
-def get_cl_all_ell(t1, t2):
-    cl = ccl.angular_cl(csm, t1, t2, ells)
-    cli = interp1d(ells, np.log(cl))
-    return np.exp(cli(ls_all))
-
-csm=ccl.Cosmology(Omega_b=0.05,
-                  Omega_c=0.25,
-                  sigma8=0.8,
-                  h=0.67,
-                  n_s=0.96,
-                  Omega_k=0)
-
-u = UHECRs("data/att_2.4_proton.dat", n_crs, 2.)
-g = Gals("2MRS", n_gal)
+cosmo=ccl.Cosmology(Omega_b=0.05,
+                    Omega_c=0.25,
+                    sigma8=0.8,
+                    h=0.67,
+                    n_s=0.96,
+                    Omega_k=0)
 
 if plot_nz:
     zz = np.linspace(0.000001, 0.15, 1024)
     plt.figure()
-    nz = g.get_nz(zz)
+    nz = g.get_nz(zz, cosmo)
     plt.plot(zz, nz/np.amax(nz), 'k-', label=r'$\phi_g,\,\,{\rm 2MRS}$')
-    for Ecut, c in zip(['60', '50', '40'],['r','b','y']):
-        nz = u.get_nz(zz, Ecut, csm)
-        plt.plot(zz, nz/np.amax(nz), c+'-',
-                 label=r'$\phi_{\rm CR},\,\,E_{\rm cut}= %s\,{\rm EeV}$' % Ecut)
+    for u, c in zip(us, cols):
+        nz = u.get_nz(zz, cosmo)
+        plt.plot(zz, nz/np.amax(nz), '-', color=c,
+                 label=r'$\phi_{\rm CR},\,\,E_{\rm cut}= %d\,{\rm EeV}$' % u.E_cut)
     plt.legend(loc='upper right', fontsize=13)
     plt.gca().tick_params(labelsize="large")
     plt.xlim([0,0.15])
     plt.ylim([0,1.1])
     plt.xlabel(r'$z$', fontsize=15)
     plt.ylabel(r'$\phi(z)\,\,({\rm normalized})$', fontsize=15)
-    plt.savefig("figures/phi.pdf", bbox_inches='tight')
+    plt.savefig("figures/phi_v2.pdf", bbox_inches='tight')
 
 if plot_cl_nl:
-    tracer_gal = g.get_tracer(csm, bias=1.5)
-    def get_cl(bias_cr, type='auto'):
-        tracer_crs = u.get_tracer(csm, '60', bias=bias_cr)
-        bl = 1#u.get_beam(ls_all)
-        if type=='auto':
-            cl = get_cl_all_ell(tracer_crs, tracer_crs) * bl**2
-        else:
-            cl = get_cl_all_ell(tracer_gal, tracer_crs) * bl
-        return cl
     plt.figure()
-    plt.fill_between(ls_all, get_cl(1.,'auto'), get_cl(2.,'auto'),
-                     alpha=0.5, color='#FF1A00',
-                     label=r'${\cal S}^{\rm CR\,CR}_\ell$')
-    plt.fill_between(ls_all, get_cl(1.,'cross'), get_cl(2.,'cross'),
-                     alpha=0.5, color='#1A21FF',
-                     label=r'${\cal S}^{g\,{\rm CR}}_\ell$')
-    plt.plot(ls_all, np.ones_like(ls_all) * 4 * np.pi / 1E2, 'k-',
-             label=r'${\cal N}^{\rm CR\,CR}_\ell,\,\,(N_{\rm CR}=10^2)$')
-    plt.plot(ls_all, np.ones_like(ls_all) * 4 * np.pi / 1E3, 'k--',
-             label=r'${\cal N}^{\rm CR\,CR}_\ell,\,\,(N_{\rm CR}=10^3)$')
-    plt.plot(ls_all, np.ones_like(ls_all) * 4 * np.pi / 1E4, 'k-.',
-             label=r'${\cal N}^{\rm CR\,CR}_\ell,\,\,(N_{\rm CR}=10^4)$')
+    for u, c in zip(us, cols):
+        plt.fill_between(ls_all,
+                         get_cl(ls_all, cosmo, u, 1.),
+                         get_cl(ls_all, cosmo, u, 2.),
+                         alpha=0.5, color=c,
+                         label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+        plt.plot(ls_all, u.get_nl(n_ells=len(ls_all)), '--', color=c)
+    plt.ylim([5E-6, 5])
     plt.xlim([np.amin(ls_all), np.amax(ls_all)])
-    plt.ylim([6E-6, 0.5])
     plt.loglog()
     plt.gca().tick_params(labelsize="large")
-    plt.legend(loc='lower left', fontsize=13, ncol=2, labelspacing=0.1)
-    plt.xlabel(r'$\ell$', fontsize=15)
-    plt.ylabel(r'$C_\ell$', fontsize=15)
-    plt.savefig("figures/cl_nl.pdf", bbox_inches='tight')
-
-if plot_cl_el:
-    tracer_gal = g.get_tracer(csm, bias=1.5)
-    tracer_crs = u.get_tracer(csm, '60', bias=1.5)
-    bl = u.get_beam(ls_all)
-    sl_cc = get_cl_all_ell(tracer_crs, tracer_crs) * bl**2
-    sl_gc = get_cl_all_ell(tracer_gal, tracer_crs) * bl
-    sl_gg = get_cl_all_ell(tracer_gal, tracer_gal)
-    nl_cc_2 = np.ones_like(sl_cc) * 4 * np.pi / 1E2
-    nl_cc_3 = np.ones_like(sl_cc) * 4 * np.pi / 1E3
-    nl_gc = np.zeros_like(sl_gc)
-    nl_gg = np.ones_like(sl_gg) * 4 * np.pi / 1E5
-    cl_cc_2 = sl_cc + nl_cc_2
-    cl_cc_3 = sl_cc + nl_cc_3
-    cl_gc = sl_gc + nl_gc
-    cl_gg = sl_gg + nl_gg
-    el_cc_2 = np.sqrt(2 * cl_cc_2**2 / (2*ls_all + 1))
-    el_gc_2 = np.sqrt((cl_cc_2 * cl_gg + cl_gc**2) / (2*ls_all + 1))
-    el_cc_3 = np.sqrt(2 * cl_cc_3**2 / (2*ls_all + 1))
-    el_gc_3 = np.sqrt((cl_cc_3 * cl_gg + cl_gc**2) / (2*ls_all + 1))
-    el_gg = np.sqrt(2 * cl_gg**2 / (2*ls_all + 1))
-
-    plt.figure()
-    plt.fill_between(ls_all, sl_gc - el_gc_2, sl_gc + el_gc_2,
-                     alpha=0.8, color='#8488FF', label=r'$N_{\rm CR}=10^2$')
-    plt.fill_between(ls_all, sl_gc - el_gc_3, sl_gc + el_gc_3,
-                     alpha=0.5, color='#1A21FF', label=r'$N_{\rm CR}=10^3$')
-    plt.plot(ls_all, sl_gc, 'k-')
-    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
-    plt.ylim([6E-6, 0.5])
-    plt.gca().tick_params(labelsize="large")
-    plt.loglog()
-    plt.legend(loc='upper right', fontsize=13, ncol=2, labelspacing=0.1)
-    plt.xlabel(r'$\ell$', fontsize=15)
-    plt.ylabel(r'${\cal S}^{g\,{\rm CR}}_\ell$', fontsize=15)
-    plt.savefig("figures/cl_el_auto.pdf", bbox_inches='tight')
-
-    plt.figure()
-    plt.fill_between(ls_all, sl_cc - el_cc_2, sl_cc + el_cc_2,
-                     alpha=0.8, color='#FFAAAA', label=r'$N_{\rm CR}=10^2$')
-    plt.fill_between(ls_all, sl_cc - el_cc_3, sl_cc + el_cc_3,
-                     alpha=0.5, color='#FF1A00', label=r'$N_{\rm CR}=10^3$')
-    plt.plot(ls_all, sl_cc, 'k-')
-    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
-    plt.ylim([6E-6, 0.5])
-    plt.gca().tick_params(labelsize="large")
-    plt.loglog()
-    plt.legend(loc='upper right', fontsize=13, ncol=2, labelspacing=0.1)
+    plt.legend(loc='lower left', fontsize=13, labelspacing=0.1)
     plt.xlabel(r'$\ell$', fontsize=15)
     plt.ylabel(r'${\cal S}^{\rm CR\,CR}_\ell$', fontsize=15)
-    plt.savefig("figures/cl_el_cross.pdf", bbox_inches='tight')
+    plt.savefig("figures/cl_nl_cc.pdf", bbox_inches='tight')
+
+    plt.figure()
+    for u, c in zip(us, cols):
+        plt.fill_between(ls_all,
+                         get_cl(ls_all, cosmo, u, 1., g, 1.5),
+                         get_cl(ls_all, cosmo, u, 2., g, 1.5),
+                         alpha=0.5, color=c,
+                         label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+        plt.plot(ls_all, u.get_nl(n_ells=len(ls_all)), '--', color=c)
+    plt.ylim([5E-6, 0.99])
+    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
+    plt.loglog()
+    plt.gca().tick_params(labelsize="large")
+    plt.legend(loc='lower left', fontsize=13, labelspacing=0.1)
+    plt.xlabel(r'$\ell$', fontsize=15)
+    plt.ylabel(r'${\cal S}^{g,{\rm CR}}_\ell$', fontsize=15)
+    plt.savefig("figures/cl_nl_gc.pdf", bbox_inches='tight')
+
+if plot_cl_el:
+    plt.figure()
+    for u, c in zip(us, cols):
+        bl = u.get_beam(ls_all)
+        sl_cc = get_cl(ls_all, cosmo, u, 1.5) * bl**2
+        nl_cc = u.get_nl()
+        cl_cc = sl_cc + nl_cc
+        el_cc = np.sqrt(2 * cl_cc**2 / (2*ls_all + 1))
+        plt.fill_between(ls_all, sl_cc - el_cc, sl_cc+el_cc,
+                         alpha=0.5, color=c,
+                         label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+        plt.plot(ls_all, sl_cc, '-', color=c)
+    plt.ylim([5E-6, 4])
+    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
+    plt.loglog()
+    plt.gca().tick_params(labelsize="large")
+    plt.legend(loc='lower left', fontsize=13, labelspacing=0.1)
+    plt.xlabel(r'$\ell$', fontsize=15)
+    plt.ylabel(r'${\cal S}^{\rm CR\,CR}_\ell$', fontsize=15)
+    plt.savefig("figures/cl_el_cc.pdf", bbox_inches='tight')
+
+    plt.figure()
+    for u, c in zip(us, cols):
+        bl = u.get_beam(ls_all)
+        sl_cc = get_cl(ls_all, cosmo, u, 1.5) * bl**2
+        sl_gc = get_cl(ls_all, cosmo, u, 1.5, g, 1.5) * bl
+        sl_gg = get_cl(ls_all, cosmo, g, 1.5)
+        nl_cc = u.get_nl()
+        nl_gg = g.get_nl()
+        cl_cc = sl_cc + nl_cc
+        cl_gg = sl_gg + nl_gg
+        cl_gc = sl_gc
+        el_gc = np.sqrt((cl_cc * cl_gg + cl_gc**2) / (2*ls_all + 1))
+        plt.fill_between(ls_all, sl_gc - el_gc, sl_gc+el_gc,
+                         alpha=0.5, color=c,
+                         label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+        plt.plot(ls_all, sl_gc, '-', color=c) 
+    plt.ylim([5E-6, 0.4])
+    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
+    plt.loglog()
+    plt.gca().tick_params(labelsize="large")
+    plt.legend(loc='lower left', fontsize=13, labelspacing=0.1)
+    plt.xlabel(r'$\ell$', fontsize=15)
+    plt.ylabel(r'${\cal S}^{g,{\rm CR}}_\ell$', fontsize=15)
+    plt.savefig("figures/cl_el_cc.pdf", bbox_inches='tight')
 
 if plot_sn:
-    tracer_gal = g.get_tracer(csm, bias=1.5)
-    tracer_crs = u.get_tracer(csm, '60', bias=1.5)
-    bl = u.get_beam(ls_all)
-    sl_cc = get_cl_all_ell(tracer_crs, tracer_crs) * bl**2
-    sl_gc = get_cl_all_ell(tracer_gal, tracer_crs) * bl
-    sl_gg = get_cl_all_ell(tracer_gal, tracer_gal)
-    nl_cc_2 = np.ones_like(sl_cc) * 4 * np.pi / 1E2
-    nl_cc_3 = np.ones_like(sl_cc) * 4 * np.pi / 1E3
-    nl_gc = np.zeros_like(sl_gc)
-    nl_gg = np.ones_like(sl_gg) * 4 * np.pi / 1E5
-    cl_cc_2 = sl_cc + nl_cc_2
-    cl_cc_3 = sl_cc + nl_cc_3
-    cl_gc = sl_gc + nl_gc
-    cl_gg = sl_gg + nl_gg
-
-    dcl_A = np.array([2 * sl_cc])
-    cov_A_2 = np.array([[2 * cl_cc_2**2 / (2*ls_all + 1)]])
-    cov_A_3 = np.array([[2 * cl_cc_3**2 / (2*ls_all + 1)]])
-    dcl_X = np.array([sl_gc])
-    cov_X_2 = np.array([[(cl_cc_2 * cl_gg + cl_gc**2) / (2*ls_all + 1)]])
-    cov_X_3 = np.array([[(cl_cc_3 * cl_gg + cl_gc**2) / (2*ls_all + 1)]])
-    dcl_T = np.array([2 * sl_cc, sl_gc, 0 * sl_gg])
-    cov_T_2 = np.array([[2*cl_cc_2**2 / (2*ls_all + 1),
-                         2*cl_cc_2*cl_gc / (2*ls_all + 1),
-                         2*cl_gc**2 / (2*ls_all + 1)],
-                        [2*cl_cc_2*cl_gc / (2*ls_all + 1),
-                         (cl_cc_2*cl_gg + cl_gc**2) / (2*ls_all + 1),
-                         2*cl_gg*cl_gc / (2*ls_all + 1)],
-                        [2*cl_gc**2 / (2*ls_all + 1),
-                         2*cl_gg*cl_gc / (2*ls_all + 1),
-                         2*cl_gg**2 / (2*ls_all + 1)]])
-    cov_T_3 = np.array([[2*cl_cc_3**2 / (2*ls_all + 1),
-                         2*cl_cc_3*cl_gc / (2*ls_all + 1),
-                         2*cl_gc**2 / (2*ls_all + 1)],
-                        [2*cl_cc_3*cl_gc / (2*ls_all + 1),
-                         (cl_cc_3*cl_gg + cl_gc**2) / (2*ls_all + 1),
-                         2*cl_gg*cl_gc / (2*ls_all + 1)],
-                        [2*cl_gc**2 / (2*ls_all + 1),
-                         2*cl_gg*cl_gc / (2*ls_all + 1),
-                         2*cl_gg**2 / (2*ls_all + 1)]])
-
-    def get_sn_l(d, c):
+    def get_sn(d, c, reverse=False):
         cc = np.transpose(c, axes=[2,0,1])  # [Nl, Nt, Nt]
         dd = d.T  # [Nl, Nt]
         idd = np.linalg.solve(cc, dd) # [Nl, Nt]
-        return np.sum(dd * idd, axis=1)
-    sn2_A_2_l = get_sn_l(dcl_A, cov_A_2)
-    sn2_A_3_l = get_sn_l(dcl_A, cov_A_3)
-    sn2_X_2_l = get_sn_l(dcl_X, cov_X_2)
-    sn2_X_3_l = get_sn_l(dcl_X, cov_X_3)
-    sn2_T_2_l = get_sn_l(dcl_T, cov_T_2)
-    sn2_T_3_l = get_sn_l(dcl_T, cov_T_3)
-
-    sn_A_2_c = np.sqrt(np.cumsum(sn2_A_2_l))
-    sn_A_3_c = np.sqrt(np.cumsum(sn2_A_3_l))
-    sn_X_2_c = np.sqrt(np.cumsum(sn2_X_2_l))
-    sn_X_3_c = np.sqrt(np.cumsum(sn2_X_3_l))
-    sn_T_2_c = np.sqrt(np.cumsum(sn2_T_2_l))
-    sn_T_3_c = np.sqrt(np.cumsum(sn2_T_3_l))
+        sn_l = np.sum(dd * idd, axis=1) # [Nl]
+        if reverse:
+            sn = np.sqrt(np.cumsum(sn_l[::-1]))[::-1]
+        else:
+            sn = np.sqrt(np.cumsum(sn_l))
+        return sn
     
-    plt.figure()
-    plt.fill_between(ls_all, sn_A_2_c, sn_A_3_c, alpha=0.5, color='#FF1A00',
-                     label='Auto-only')
-    plt.fill_between(ls_all, sn_X_2_c, sn_X_3_c, alpha=0.5, color='#1A21FF',
-                     label='Cross-only')
-    plt.fill_between(ls_all, sn_T_2_c, sn_T_3_c, alpha=0.5, color='#AAAAAA',
-                     label='All data')
-    plt.plot(ls_all, 3.*np.ones_like(ls_all), 'k--', lw=1)
-    plt.xlim([np.amin(ls_all), np.amax(ls_all)])
-    plt.loglog()
-    plt.gca().tick_params(labelsize="large")
-    plt.legend(loc='lower right', fontsize=13)
+    plt.figure(1001)
+    plt.figure(1002)
+    for u, c in zip(us, cols):
+        bl = u.get_beam(ls_all)
+        sl_cc = get_cl(ls_all, cosmo, u, 1.5) * bl**2
+        sl_gc = get_cl(ls_all, cosmo, u, 1.5, g, 1.5) * bl
+        sl_gg = get_cl(ls_all, cosmo, g, 1.5)
+        nl_cc = u.get_nl()
+        nl_gg = g.get_nl()
+        cl_cc = sl_cc + nl_cc
+        cl_gc = sl_gc
+        cl_gg = sl_gg + nl_gg
+        dcl_A = np.array([2 * sl_cc])
+        cov_A = np.array([[2 * cl_cc**2 / (2*ls_all + 1)]])
+        dcl_X = np.array([sl_gc])
+        cov_X = np.array([[(cl_cc * cl_gg + cl_gc**2) / (2*ls_all + 1)]])
+        dcl_T = np.array([2* sl_cc, sl_gc, 0 * sl_gg])
+        cov_T = np.array([[2*cl_cc**2 / (2*ls_all + 1),
+                           2*cl_cc*cl_gc / (2*ls_all + 1),
+                           2*cl_gc**2 / (2*ls_all + 1)],
+                          [2*cl_cc*cl_gc / (2*ls_all + 1),
+                           (cl_cc * cl_gg + cl_gc**2) / (2*ls_all + 1),
+                           2*cl_gg*cl_gc / (2*ls_all + 1)],
+                          [2*cl_gc**2/ (2*ls_all + 1),
+                           2*cl_gg*cl_gc / (2*ls_all + 1),
+                           2*cl_gg**2 / (2*ls_all + 1)]])
+        sn_A_lmax = get_sn(dcl_A, cov_A, reverse=False)
+        sn_A_lmin = get_sn(dcl_A, cov_A, reverse=True)
+        sn_X_lmax = get_sn(dcl_X, cov_X, reverse=False)
+        sn_X_lmin = get_sn(dcl_X, cov_X, reverse=True)
+        sn_T_lmax = get_sn(dcl_T, cov_T, reverse=False)
+        sn_T_lmin = get_sn(dcl_T, cov_T, reverse=True)
+
+        plt.figure(1001)
+        plt.plot(ls_all, sn_A_lmax, '-.', color=c)
+        plt.plot(ls_all, sn_X_lmax, '--', color=c)
+        plt.plot(ls_all, sn_T_lmax, '-', color=c,
+                 label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+        plt.figure(1002)
+        plt.plot(ls_all, sn_A_lmin, '-.', color=c)
+        plt.plot(ls_all, sn_X_lmin, '--', color=c)
+        plt.plot(ls_all, sn_T_lmin, '-', color=c,
+                 label=r'$E_{\rm cut}=%d\,{\rm EeV}$' % u.E_cut)
+
+    for i in [1001, 1002]:
+        plt.figure(i)
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.ylim([0.5, 100])
+        plt.xlim([np.amin(ls_all), np.amax(ls_all)])
+        plt.plot([-1,-1],[-1,-1],'k-.', label=r'${\rm Auto-only}$')
+        plt.plot([-1,-1],[-1,-1],'k--', label=r'${\rm Cross-only}$')
+        plt.plot([-1,-1],[-1,-1],'k-', label=r'${\rm Auto + cross}$')
+        plt.plot(ls_all, 3.*np.ones_like(ls_all), 'k--', lw=1)
+        plt.gca().tick_params(labelsize="large")
+    plt.figure(1001)
     plt.xlabel(r'$\ell_{\rm max}$', fontsize=15)
-    plt.ylabel(r'$(S / N)_{\ell \leq \ell_{\rm max}}$', fontsize=15)
-    plt.savefig("figures/sn_lmax.pdf", bbox_inches='tight')
-
-    sn_A_2_c = np.sqrt(np.cumsum(sn2_A_2_l[::-1]))[::-1]
-    sn_A_3_c = np.sqrt(np.cumsum(sn2_A_3_l[::-1]))[::-1]
-    sn_X_2_c = np.sqrt(np.cumsum(sn2_X_2_l[::-1]))[::-1]
-    sn_X_3_c = np.sqrt(np.cumsum(sn2_X_3_l[::-1]))[::-1]
-    sn_T_2_c = np.sqrt(np.cumsum(sn2_T_2_l[::-1]))[::-1]
-    sn_T_3_c = np.sqrt(np.cumsum(sn2_T_3_l[::-1]))[::-1]
-    
-    plt.figure()
-    plt.fill_between(ls_all, sn_A_2_c, sn_A_3_c, alpha=0.5, color='#FF1A00',
-                     label='Auto-only')
-    plt.fill_between(ls_all, sn_X_2_c, sn_X_3_c, alpha=0.5, color='#1A21FF',
-                     label='Cross-only')
-    plt.fill_between(ls_all, sn_T_2_c, sn_T_3_c, alpha=0.5, color='#AAAAAA',
-                     label='All data')
-    plt.plot(ls_all, 3.*np.ones_like(ls_all), 'k--', lw=1)
-    plt.xlim([np.amin(ls_all), 300])
-    plt.ylim([0.5, 36])
-    plt.loglog()
-    plt.gca().tick_params(labelsize="large")
-    plt.legend(loc='lower left', fontsize=13)
+    plt.ylabel(r'$\left(\frac{S}{N}\right)_{\ell \leq \ell_{\rm max}}$', fontsize=15)
+    plt.legend(loc='upper left', fontsize=13, labelspacing=0.1, ncol=2)
+    plt.savefig("figures/sn_lmax_v2.pdf", bbox_inches='tight')
+    plt.figure(1002)
     plt.xlabel(r'$\ell_{\rm min}$', fontsize=15)
-    plt.ylabel(r'$(S / N)_{\ell \geq \ell_{\rm min}}$', fontsize=15)
-    plt.savefig("figures/sn_lmin.pdf", bbox_inches='tight')
-
+    plt.ylabel(r'$\left(\frac{S}{N}\right)_{\ell \geq \ell_{\rm min}}$', fontsize=15)
+    plt.legend(loc='upper right', fontsize=13, labelspacing=0.1, ncol=2)
+    plt.savefig("figures/sn_lmin_v2.pdf", bbox_inches='tight')
 plt.show()
--- utils.py
+++ utils.py
@@ -3,83 +3,92 @@ import matplotlib.pyplot as plt
 import pyccl as ccl
 from scipy.interpolate import interp1d
 
-class UHECRs(object):
-    def __init__(self, att_fname, n_crs, fwhm_deg):
-        self.d_att = self._get_att_data(att_fname)
-        self.n_crs = n_crs
-        self.sigma_beam = np.radians(fwhm_deg) / 2.355
+class DiscreteTracer(object):
+    def __init__(self, n_obj):
+        self.n_obj = n_obj
 
-    def get_beam(self, ls):
-        return np.exp(-0.5 * self.sigma_beam**2 * ls * (ls+1))
+    def get_nz(self, z, cosmo):
+        raise NotImplementedError("N(z) not implemented for this tracer")
 
-    def get_nz(self, z, E_cut, cosmo):
-        att = self._get_att_extrapolated(E_cut, z)
-        # Add (1+z) * H(z) factor
-        a = 1./(1+z)
-        nz = att * a / ccl.h_over_h0(cosmo, a)
-        return nz
-        
-    def get_tracer(self, cosmo, E_cut, bias=1, z_max=0.15, nz=1024):
+    def get_tracer(self, cosmo, bias, z_max=0.15, nz=1024):
         zarr = np.linspace(0.000001, z_max, nz)
         bzarr = np.ones(nz) * bias
-        nzarr = self.get_nz(zarr, E_cut, cosmo)
+        nzarr = self.get_nz(zarr, cosmo)
         return ccl.NumberCountsTracer(cosmo, False,
                                       dndz=(zarr, nzarr),
                                       bias=(zarr, bzarr))
 
-    def _get_att_data(self, fname):
-        chi, z, ecut, alpha = np.loadtxt(fname, unpack=True)
-        ecut = ecut.astype(int)
+    def get_beam(self, ls):
+        return np.exp(-0.5 * self.sigma_beam**2 * ls * (ls+1))
 
-        data = {}
-        for e in np.unique(ecut):
-            ids = ecut == e
-            data['%d'%e] = {'chi': chi[ids], 'z': z[ids], 'att': alpha[ids]}
-        return data
-
-    def _get_att_extrapolated(self, E_cut, z_out):
-        d_att = self.d_att[E_cut]
-        att_f = interp1d(np.log(d_att['z']),
-                         np.log(d_att['att']),
-                         bounds_error=False,
-                         fill_value=np.log(d_att['att'][0]))
-        att_out = np.exp(att_f(np.log(z_out)))
-        tilt = np.log(d_att['att'][-1]/d_att['att'][-2])/ \
-               np.log(d_att['z'][-1]/d_att['z'][-2])
-        id_out = np.where(z_out > d_att['z'][-1])[0]
-        att_out[id_out]=d_att['att'][-1] * (z_out[id_out]/d_att['z'][-1])**tilt
-        return att_out
-
-class Gals(object):
-    def __init__(self, nz, n_gals):
-        if nz=='2MRS':
-            self.nz = self._get_nz_2mrs()
+    def get_nl(self, n_ells=None):
+        nl_val = 4*np.pi/self.n_obj
+        if n_ells is not None:
+            return nl_val * np.ones(n_ells)
         else:
-            self.nz = nz
-        self.n_gals = n_gals
-        self.nzf = interp1d(self.nz[0], self.nz[1],
-                            bounds_error=False,
-                            fill_value=0)
+            return nl_val
 
-    def get_nz(self, z):
-        return self.nzf(z)
 
-    def _get_nz_2mrs(self, z_max=0.15, nz=1024):
+class UHECRs(DiscreteTracer):
+    def __init__(self, E_cut, att_fname, n_crs, fwhm_deg,
+                 z_max=0.15, nz=1024):
+        self.E_cut = E_cut
+        self.n_obj = n_crs
+        self.sigma_beam = np.radians(fwhm_deg) / 2.355
+        self.att_f = self._get_att_function(att_fname, E_cut,
+                                            z_max=z_max, nz=nz)
+
+    def get_nz(self, z, cosmo):
+        att = self.att_f(z)
+        # Add (1+z) * H(z) factor
+        a = 1./(1+z)
+        nz = att * a / ccl.h_over_h0(cosmo, a)
+        return nz
+
+    def _get_att_function(self, fname, E_cut, z_max=0.15, nz=1024):
+        z, Ecut, att = np.loadtxt(fname, skiprows=1, unpack=True)
+        Ecut = (Ecut*1E-18).astype(int)
+        Ecut_list = np.unique(Ecut)
+        if E_cut not in Ecut_list:
+            raise ValueError("Input E_cut = %d EeV not in attenuation file" % E_cut)
+        msk = Ecut == E_cut
+        return interp1d(z[msk], att[msk], bounds_error=False, fill_value=0)
+
+class Gals(DiscreteTracer):
+    def __init__(self, nz_arr, n_gals,
+                 z_max=0.15, nz=1024):
+        if nz_arr=='2MRS':
+            self.nz_f = self._nz_2mrs
+        else:
+            self.nz_f = interp1d(nz_arr[0], nz_arr[1],
+                                 bounds_error=False, fill_value=0)
+        self.n_obj = n_gals
+        self.sigma_beam = 0
+
+    def get_nz(self, z, cosmo):
+        return self.nz_f(z)
+
+    def _nz_2mrs(self, z):
         # From 1706.05422
-        z0 = 0.0266
         m = 1.31
         beta = 1.64
-        zarr = np.linspace(0.000001, z_max, nz)
-        xarr = zarr / z0
-        nzarr = xarr**m * np.exp(-xarr**beta)
-        return (zarr, nzarr)
+        x = z / 0.0266
+        return x**m * np.exp(-x**beta)
 
-    def get_tracer(self, cosmo, bias=1.):
-        zarr = self.nz[0]
-        bzarr = np.ones_like(zarr) * bias
-        return ccl.NumberCountsTracer(cosmo, False,
-                                      dndz=self.nz,
-                                      bias=(zarr, bzarr))
+def get_cl(ell, cosmo, t1, b1, t2=None, b2=None, ell_pivots=None):
+    if ell_pivots is None:
+        lmx = np.amax(ell)+1
+        ell_pivots = np.unique(np.geomspace(2, lmx,
+                                            int(100 * np.log10(lmx) / 3.)).astype(int)).astype(float)
+    if b2 is None:
+        b2 = b1
 
-    def get_beam(self, ls):
-        return np.ones(len(ls))
+    ct1 = t1.get_tracer(cosmo, b1)
+    if t2 is None:
+        ct2 = ct1
+    else:
+        ct2 = t2.get_tracer(cosmo, b2)
+        
+    cl = ccl.angular_cl(cosmo, ct1, ct2, ell_pivots)
+    cli = interp1d(np.log(ell_pivots), np.log(cl))
+    return np.exp(cli(np.log(ell)))
